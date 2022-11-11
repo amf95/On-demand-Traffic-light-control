10b5cf2dedc983e505da1e990c2d77bede0bb41c
amf95 <31721588+amf95@users.noreply.github.com>
2022-11-11 21:36:12 +0200

    Delete On-demand Traffic light control directory
    updated with atmel studio code

--- On-demand Traffic light control/Application/application.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef APPLICATION_H_
-#define APPLICATION_H_
-
-void APP_init();
-void APP_start();
-
-#endif
--- On-demand Traffic light control/types.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef TYPES_H_
-#define TYPES_H_
-
-typedef unsigned char uint8_t;
-
-#endif
