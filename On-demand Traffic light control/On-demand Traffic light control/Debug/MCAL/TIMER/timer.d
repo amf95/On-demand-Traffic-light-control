f096294ba8db5ce531d80d7f866e36be426bd9ad
amf95 <31721588+amf95@users.noreply.github.com>
2022-11-14 23:59:06 +0200

    Delete On-demand Traffic light control directory
    reupload final version without long press block

--- On-demand Traffic light control/On-demand Traffic light control/Debug/MCAL/TIMER/timer.d
+++
@@ -1,9 +0,0 @@
-MCAL/TIMER/timer.d MCAL/TIMER/timer.o: ../MCAL/TIMER/timer.c \
- ../MCAL/TIMER/timer.h ../MCAL/TIMER/../registers.h \
- ../MCAL/TIMER/../types.h
-
-../MCAL/TIMER/timer.h:
-
-../MCAL/TIMER/../registers.h:
-
-../MCAL/TIMER/../types.h:
